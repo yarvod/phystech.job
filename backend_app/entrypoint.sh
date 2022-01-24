d2d1f7498ed6693e32ec9e62a5fe90f5e8a90032
yarvod <yarvod@icloud.com>
2022-01-24 21:39:47 +0300

    Initial commit

--- backend_app/entrypoint.sh
+++ backend_app/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-exec gunicorn mysite.wsgi:application -b 0.0.0.0:8000 --reload
+exec gunicorn backend_app.wsgi:application -b 0.0.0.0:8000 --reload
