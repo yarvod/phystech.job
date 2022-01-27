9c683edffec6cd8670afa55f9d1900c4c71fc7a8
atepart <art.tyapkha@mail.ru>
2022-01-27 22:14:05 +0300

    header__item positions

--- frontend_app/src/components/Header.vue
+++ frontend_app/src/components/Header.vue
@@ -1,20 +1,41 @@
 <template>
-<div class="header">
-      <a href="/"><h1>phystech.job</h1></a>
-      <form class="form">
-        <select>
-          <option value="1">Долгопрудный</option>
-          <option value="2">Жуковский</option>
-          <option value="3">Москва</option>
-          <option value="4">По-хуй</option>
-        </select>
-
-      </form>
-      <div class="help">
-        HELP here
+  <div class="header">
+    <div class="header__menu">
+      <div class="header__section headgeneral">
+        <a href="/" class="header__item headlogo" title="Домашняя страница" tabindex="0">
+          <h1>PJ</h1>
+        </a>
+        <form class="header__item headloc" title="Выберете регион">
+          <select>
+            <option value="1" tabindex="1">Долгопрудный</option>
+            <option value="2" tabindex="2">Жуковский</option>
+            <option value="3" tabindex="3">Москва</option>
+            <option value="4" tabindex="4">Похую</option>
+          </select>
+        </form>
+        <a href="/help" class="header__item headhelp" title="Помощь" tabindex="5">
+          <p>HELP here</p>
+        </a>
+      </div>
+      <div class="header__section headaccount">
+        <div class="header__item headpa">
+          <form action="/account">
+            <button type="link" title="Личный кабинет" tabindex="6">Личный кабинет</button>
+          </form>
+        </div>
+        <!-- <div class="header__enter">
+          <form action="/enter">
+            <button type="link" title="Войти в личный кабинет" tabindex="#">Войти</button>
+          </form>
+        </div>
+        <div class="header__reg">
+          <form action="/registration">
+            <button type="link" title="Создать учётную" tabindex="#">Регистрация</button>
+          </form>
+        </div> -->
       </div>
     </div>
-      <hr style="margin: 0; width: 100%; position: fixed; top: 0; left: 0">
+  </div>
 </template>
 
 <script>
@@ -26,25 +47,37 @@ export default {
 <style scoped>
 .header {
   display: flex;
+  height: 64px;
+  min-width: 100%;
+  position: sticky;
+  border-bottom: 1px solid skyblue;
+  background: white;
+}
+
+.header__menu {
+  display: flex;
+  min-width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header__section {
+  display: flex;
   flex-direction: row;
-  width: 100%;
-  margin: 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-
-  background: antiquewhite;
 }
 
-.form {
-  align-content: center;
-  position: relative;
-  margin: 0 15px;
+.header__item {
+  padding: 2px;
+  margin: 5px;
+  color: skyblue;
+  border: 1px solid black;
 }
 
-.help {
-  align-content: center;
-  position: relative;
-  margin: 0 15px;
+.headgeneral {
+  margin-left: 10px;
+}
+
+.headaccount {
+  margin-right: 10px;
 }
 </style>
