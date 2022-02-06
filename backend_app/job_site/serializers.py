17b43aa7735465c718736a812a6a0517e6592f83
yarvod <yarvod@icloud.com>
2022-02-07 01:15:56 +0300

    add new app

---
+++ backend_app/job_site/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    model = Post
+    fields = ('id', 'title', 'body', 'created')
---
+++ backend_app/job_site/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+
+    queryset = Post.objects.all().order_by('-created')
+    serializer_class = PostSerializer
+
