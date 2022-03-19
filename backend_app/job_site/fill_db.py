5c6045929d57e23d85df2748cb3353a0cbad421b
yarvod <yarvod@icloud.com>
2022-03-19 08:37:13 +0300

    add default fill db func

---
+++ backend_app/job_site/fill_db.py
@@ -0,0 +1,31 @@
+from .models import *
+from django.contrib.auth.models import User
+
+
+def fill_db():
+    user1 = User.objects.update_or_create(username='user1', defaults=dict(password='user1_pass_phystech'))[0]
+
+    user2 = User.objects.update_or_create(username='user2', defaults=dict(password='user2_pass_phystech'))[0]
+
+    employee = Employee.objects.update_or_create(user=user1)[0]
+
+    employer = Employer.objects.update_or_create(user=user2)[0]
+    employer.company_name = 'Company 1'
+
+    category = Category.objects.update_or_create(title='IT', slug='it')[0]
+
+    for i in range(6):
+        fields = dict(
+            employee=employee,
+            body=f'Resume {i}'+f'Resume {i}'+f'Resume {i}',
+            category=category
+        )
+        Resume.objects.update_or_create(title=f'Resume {i}', defaults=fields)
+
+    for i in range(6):
+        fields = dict(
+            employer=employer,
+            body=f'Vacancy {i}'+f'Vacancy {i}'+f'Vacancy {i}',
+            category=category
+        )
+        Vacancy.objects.update_or_create(title=f'Vacancy {i}', defaults=fields)
