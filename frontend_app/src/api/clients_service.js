d4a84f7ab9ae579a58346f6872b1c80a3ddee6a8
yarvod <yar.vod.ole@gmail.com>
2022-05-07 02:08:10 +0300

    add clients_service.js

---
+++ frontend_app/src/api/clients_service.js
@@ -0,0 +1,13 @@
+import Api from '@/api/Api'
+
+export default {
+  async getClients () {
+    return await Api().get('/clients/')
+  },
+  async getClient (id) {
+    return await Api().get(`/clients/${id}/`)
+  },
+  async setFavoriteService (id, f_s_id) {
+    return await Api().put(`/clients/${id}/`, {favorite_services_id: [f_s_id]})
+  },
+}
