4957f180e748a47fdbe738b7628c87d1eee62734
yarvod <yar.vod.ole@gmail.com>
2022-05-13 04:01:45 +0300

    upd admin

--- backend_app/job_site/admin.py
+++ backend_app/job_site/admin.py
@@ -39,11 +39,11 @@ class BaseUserAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='user__first_name', description='First Name')
     def get_first_name(self, obj):
-        return obj.user.username
+        return obj.user.first_name
 
     @admin.display(ordering='user__last_name', description='Second Name')
     def get_last_name(self, obj):
-        return obj.user.first_name
+        return obj.user.last_name
 
     class Meta:
         abstract = True
