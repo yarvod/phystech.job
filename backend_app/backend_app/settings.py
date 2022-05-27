aa5d6dc24af4d33d490b4d2279c43af17fd9631c
yarvod <yar.vod.ole@gmail.com>
2022-05-27 19:18:37 +0300

    some fixes

--- backend_app/backend_app/settings.py
+++ backend_app/backend_app/settings.py
@@ -156,6 +156,7 @@ AUTHENTICATION_BACKENDS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
